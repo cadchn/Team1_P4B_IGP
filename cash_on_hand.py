d235c158d9a43a86613871144b21164bca92a904
Olinaa <156683939+Olinaa@users.noreply.github.com>
2024-01-24 19:41:56 +0800

    Update cash_on_hand.py

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -30,6 +30,38 @@ increase_list = []
 for difference in difference_list:
     if difference[1] < 0:
         decrease_list.append([difference[0], difference[1]*-1])
+
+
+def sort_tuple(tup, truth):
+    new_deficit_list = tup[:]
+    new_deficit_list.sort(key = lambda x:x[1], reverse = truth)
+    return new_deficit_list
+# this part onwards should be in main.py file, have to also write the code to writelines in the summary report txt file in main.py file
+sorted_deficit_list = sort_tuple(deficit_list, True)
+sorted_surplus_list = sort_tuple(surplus_list, True)
+# print(sorted_deficit_list)
+# HD = sorted_deficit_list[0][0], sorted_deficit_list[0][1]
+# SHD = sorted_deficit_list[1][0], sorted_deficit_list[1][1]
+# THD = sorted_deficit_list[2][0], sorted_deficit_list[2][1]
+# print(HD)
+deficit_list.sort(key = lambda x:x[0], reverse = False)
+# print(deficit_list)
+
+if len(surplus_list) == len(difference_list):
+    print(f"[CASH ON HAND SURPLUS] CASH ON HAND ON EACH DAY IS HIGHER THAN PREVIOUS DAY")
+    print(f"[HIGHEST NET PROFIT SURPLUS] DAY: {sorted_surplus_list[0][0]}, AMOUNT: USD{sorted_surplus_list[0][1]}")
+elif len(deficit_list) == len(difference_list):
+    print("[CASH ON HAND SURPLUS] CASH ON HAND ON EACH DAY IS LOWER THAN PREVIOUS DAY")
+    print(f"[HIGHEST CASH ON HAND DEFICIT] DAY: {sorted_deficit_list[0][0]}, AMOUNT: {sorted_deficit_list[0][1]}")
+elif len(surplus_list) == 0 and len(deficit_list) == 0:
+    print("are you even playing the game?")
+else:
+    for deficit in deficit_list:
+        print(f"[CASH ON HAND DEFICIT] DAY: {deficit[0]}, AMOUNT: USD{deficit[1]}")
+    print(f"[HIGHEST CASH ON HAND DEFICIT] DAY: {sorted_deficit_list[0][0]}, AMOUNT: {sorted_deficit_list[0][1]}")
+    print(f"[2ND HIGHEST CASH ON HAND DEFICIT] DAY: {sorted_deficit_list[1][0]}, AMOUNT: {sorted_deficit_list[1][1]}")
+    print(f"[3RD HIGHEST CASH ON HAND DEFICIT] DAY: {sorted_deficit_list[2][0]}, AMOUNT: {sorted_deficit_list[2][1]}")
+
 for difference in difference_list:
     if difference[1] > 0:
         increase_list.append([difference[0], difference[1]])
