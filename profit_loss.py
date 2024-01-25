1b14aa6b1f8d510a5b8b3d70f768413b5a3d0719
chngchngchngchng <jocelynchng06@gmail.com>
2024-01-25 19:35:19 +0800

    solved lambda issue, help with file path issue

--- profit_loss.py
+++ profit_loss.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import csv
-
+# how do you make the path accessible for all ah - joce :))))
 file_path = Path(r"C:\Users\jocel\PFB IGP\Team1_P4B_IGP\profit_and_loss.csv")
 
 with file_path.open(mode="r", encoding="UTF-8") as file:
@@ -27,47 +27,36 @@ def difference_computation():
     
 difference_computation()
 
-# from here onwards, idk if need, but this is a desparate attempt to find out always increasing, decreasing or fluctuating
 deficit_list = []
+sorted_deficit_list = []
 surplus_list = []
+sorted_surplus_list = []
 for difference in difference_list:
     if difference[1] < 0:
         deficit_list.append([difference[0], difference[1]*-1])
+        sorted_deficit_list.append([difference[1]*-1, difference[0]])
 for difference in difference_list:
     if difference[1] > 0:
         surplus_list.append([difference[0], difference[1]])
-# print(deficit_list)
+        sorted_surplus_list.append([difference[1], difference[0]])
 
-def sort_tuple(tup, truth):
-    new_deficit_list = tup[:]
-    new_deficit_list.sort(key = lambda x:x[1], reverse = truth)
-    return new_deficit_list
-
-# this part onwards should be in main.py file, have to also write the code to writelines in the summary report txt file in main.py file
-sorted_deficit_list = sort_tuple(deficit_list, True)
-sorted_surplus_list = sort_tuple(surplus_list, True)
-# print(sorted_deficit_list)
-# HD = sorted_deficit_list[0][0], sorted_deficit_list[0][1]
-# SHD = sorted_deficit_list[1][0], sorted_deficit_list[1][1]
-# THD = sorted_deficit_list[2][0], sorted_deficit_list[2][1]
-# print(HD)
-deficit_list.sort(key = lambda x:x[0], reverse = False)
-# print(deficit_list)
+sorted_deficit_list.sort(reverse=True)
+sorted_surplus_list.sort(reverse=True)
 
 if len(surplus_list) == len(difference_list):
     print(f"[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN PREVIOUS DAY")
-    print(f"[HIGHEST NET PROFIT SURPLUS] DAY: {sorted_surplus_list[0][0]}, AMOUNT: USD{sorted_surplus_list[0][1]}")
+    print(f"[HIGHEST NET PROFIT SURPLUS] DAY: {sorted_surplus_list[0][1]}, AMOUNT: USD{sorted_surplus_list[0][0]}")
 elif len(deficit_list) == len(difference_list):
     print("[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS LOWER THAN PREVIOUS DAY")
-    print(f"[HIGHEST NET PROFIT DEFICIT] DAY: {sorted_deficit_list[0][0]}, AMOUNT: {sorted_deficit_list[0][1]}")
+    print(f"[HIGHEST NET PROFIT DEFICIT] DAY: {sorted_deficit_list[0][1]}, AMOUNT: {sorted_deficit_list[0][0]}")
 elif len(surplus_list) == 0 and len(deficit_list) == 0:
     print("are you even playing the game?")
 else:
     for deficit in deficit_list:
         print(f"[NET PROFIT DEFICIT] DAY: {deficit[0]}, AMOUNT: USD{deficit[1]}")
-    print(f"[HIGHEST NET PROFIT DEFICIT] DAY: {sorted_deficit_list[0][0]}, AMOUNT: {sorted_deficit_list[0][1]}")
-    print(f"[2ND HIGHEST NET PROFIT DEFICIT] DAY: {sorted_deficit_list[1][0]}, AMOUNT: {sorted_deficit_list[1][1]}")
-    print(f"[3RD HIGHEST NET PROFIT DEFICIT] DAY: {sorted_deficit_list[2][0]}, AMOUNT: {sorted_deficit_list[2][1]}")
+    print(f"[HIGHEST NET PROFIT DEFICIT] DAY: {sorted_deficit_list[0][1]}, AMOUNT: {sorted_deficit_list[0][0]}")
+    print(f"[2ND HIGHEST NET PROFIT DEFICIT] DAY: {sorted_deficit_list[1][1]}, AMOUNT: {sorted_deficit_list[1][0]}")
+    print(f"[3RD HIGHEST NET PROFIT DEFICIT] DAY: {sorted_deficit_list[2][1]}, AMOUNT: {sorted_deficit_list[2][0]}")
 
 
 
