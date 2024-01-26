39ee45d9b3eb0ec3f24b57005be9d02fb46c7943
caden <156208642+cadchn@users.noreply.github.com>
2024-01-26 19:16:49 +0800

    new code

--- profit_loss.py
+++ profit_loss.py
@@ -1,19 +1,20 @@
 from pathlib import Path
 import csv
-# how do you make the path accessible for all ah - joce :))))
-# file_path = Path(r"C:\Users\jocel\PFB IGP\Team1_P4B_IGP\profit_and_loss.csv")
 
-file_path = Path("profit_loss.py")
-
+'''
+Read data from file into array
+'''
+file_path = Path(r"profit_and_loss.csv")
 with file_path.open(mode="r", encoding="UTF-8") as file:
     reader = csv.reader(file)
     next(reader)
 
     profit_loss = []
     for row in reader:
-        profit_loss.append([int(row[0]), float(row[4])])
+        profit_loss.append([int(row[0]), float(row[4])]) #read first and last column into the array.
 
-# print(profit_loss) 
+
+#Array to store the net profit difference 
 difference_list = []
 def difference_computation():
     '''
@@ -24,54 +25,51 @@ def difference_computation():
     for row in profit_loss:
         difference = row[1] - previous 
         previous = row[1] # previous variable will now be first index of preceding row
-        difference_list.append([row[0],difference]) # appending all difference into an empty list
+        difference_list.append([row[0], int(row[1]), int(difference)]) # appending day, net profit and the difference into an empty list
+        
     return difference_list
     
 difference_computation()
 
+
+#Find out if profit is increasing, decreasing or fluctuating
 deficit_list = []
-sorted_deficit_list = []
 surplus_list = []
-sorted_surplus_list = []
-for difference in difference_list:
-    if difference[1] < 0:
-        deficit_list.append([difference[0], difference[1]*-1])
-        sorted_deficit_list.append([difference[1]*-1, difference[0]])
-for difference in difference_list:
-    if difference[1] > 0:
-        surplus_list.append([difference[0], difference[1]])
-        sorted_surplus_list.append([difference[1], difference[0]])
+profit_loss_flag = -1
 
-sorted_deficit_list.sort(reverse=True)
-sorted_surplus_list.sort(reverse=True)
+counter = 1
 
-if len(surplus_list) == len(difference_list):
-    print(f"[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN PREVIOUS DAY")
-    print(f"[HIGHEST NET PROFIT SURPLUS] DAY: {sorted_surplus_list[0][1]}, AMOUNT: USD{sorted_surplus_list[0][0]}")
-elif len(deficit_list) == len(difference_list):
-    print("[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS LOWER THAN PREVIOUS DAY")
-    print(f"[HIGHEST NET PROFIT DEFICIT] DAY: {sorted_deficit_list[0][1]}, AMOUNT: {sorted_deficit_list[0][0]}")
-elif len(surplus_list) == 0 and len(deficit_list) == 0:
-    print("are you even playing the game?")
+# go through the list to find deficit and store in a deficit list
+for difference in difference_list:    
+    
+    if counter < len(difference_list):
+        
+        if difference_list[counter][1] < difference[1]:
+            deficit_list.append([difference_list[counter][0], difference_list[counter][2]])
+
+    counter = counter + 1
+
+# go through the list to find surplus and store in a surplus list
+counter = 1
+for difference in difference_list:    
+    
+    if counter < len(difference_list):
+
+        if difference_list[counter][1] >= difference[1]:
+            surplus_list.append([difference_list[counter][0], difference_list[counter][2]])
+
+    counter = counter + 1
+
+
+if len(surplus_list) == len(difference_list)-1:
+    profit_loss_flag = 0
+    surplus_list.sort(key=lambda x: x[1], reverse=True)  #sort by descending order so that the largest surplus is the first in list
+elif len(deficit_list) == len(difference_list)-1:
+    profit_lost_flag = 1
+    deficit_list.sort(key=lambda x: x[1], reverse=False)  #sort by ascending order so that the largest deficit is the first in list
 else:
-    for deficit in deficit_list:
-        print(f"[NET PROFIT DEFICIT] DAY: {deficit[0]}, AMOUNT: USD{deficit[1]}")
-    print(f"[HIGHEST NET PROFIT DEFICIT] DAY: {sorted_deficit_list[0][1]}, AMOUNT: {sorted_deficit_list[0][0]}")
-    print(f"[2ND HIGHEST NET PROFIT DEFICIT] DAY: {sorted_deficit_list[1][1]}, AMOUNT: {sorted_deficit_list[1][0]}")
-    print(f"[3RD HIGHEST NET PROFIT DEFICIT] DAY: {sorted_deficit_list[2][1]}, AMOUNT: {sorted_deficit_list[2][0]}")
+    profit_loss_flag = 2
+    deficit_list.sort(key=lambda x: x[1], reverse=False)  #sort by ascending order so that the largest deficit is the first in list
 
 
-
-
-# sorted_deficit_list.append(deficit_list)
-
-# deficit_summary_txt = ""
-# for i in sorted_deficit_list: 
-#     deficit_summary_txt += ','.join(i)+'\n'
-
-# # create a file path to txt file.
-# fp = Path.cwd()/"summary_report.txt"
-
-# with fp.open(mode="w", encoding="UTF-8", newline='') as file:
-#     file.write('top 3 highest deficit amount\nday occured')
 
