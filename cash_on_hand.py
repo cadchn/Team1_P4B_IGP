639e5651c3c7d78b799213f8c1791b6112536430
Olinaa <156683939+Olinaa@users.noreply.github.com>
2024-01-26 12:43:20 +0800

    Update cash_on_hand.py

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -14,9 +14,10 @@ with file_path.open(mode="r", encoding="UTF-8") as file:
 difference_list = []
 def difference_computation():
     '''
-    This function finds the difference between the consecutive values of cash on hand column. 
+    This function finds the difference between the consecutive values of net profit column. 
     '''
     previous = 0
+    
     for row in cash_on_hand:
         difference = row[1] - previous 
         previous = row[1] # previous variable will now be first index of preceding row
@@ -25,39 +26,33 @@ def difference_computation():
 
 difference_computation()
 
-decrease_list = []
-increase_list = []
+deficit_list = []
+sorted_deficit_list = []
+surplus_list = []
+sorted_surplus_list = []
 for difference in difference_list:
     if difference[1] < 0:
-        decrease_list.append([difference[0], difference[1]*-1])
+        deficit_list.append([difference[0], difference[1]*-1])
+        sorted_deficit_list.append([difference[1]*-1, difference[0]])
+for difference in difference_list:
+    if difference[1] > 0:
+        surplus_list.append([difference[0], difference[1]])
+        sorted_surplus_list.append([difference[1], difference[0]])
 
-
-def sort_tuple(tup, truth):
-    new_deficit_list = tup[:]
-    new_deficit_list.sort(key = lambda x:x[1], reverse = truth)
-    return new_deficit_list
-# this part onwards should be in main.py file, have to also write the code to writelines in the summary report txt file in main.py file
-sorted_deficit_list = sort_tuple(deficit_list, True)
-sorted_surplus_list = sort_tuple(surplus_list, True)
-# print(sorted_deficit_list)
-# HD = sorted_deficit_list[0][0], sorted_deficit_list[0][1]
-# SHD = sorted_deficit_list[1][0], sorted_deficit_list[1][1]
-# THD = sorted_deficit_list[2][0], sorted_deficit_list[2][1]
-# print(HD)
-deficit_list.sort(key = lambda x:x[0], reverse = False)
-# print(deficit_list)
+sorted_deficit_list.sort(reverse=True)
+sorted_surplus_list.sort(reverse=True)
 
 if len(surplus_list) == len(difference_list):
-    print(f"[CASH ON HAND SURPLUS] CASH ON HAND ON EACH DAY IS HIGHER THAN PREVIOUS DAY")
-    print(f"[HIGHEST NET PROFIT SURPLUS] DAY: {sorted_surplus_list[0][0]}, AMOUNT: USD{sorted_surplus_list[0][1]}")
+    print(f"[NET PROFIT SURPLUS] CASH ON HAND ON EACH DAY IS HIGHER THAN PREVIOUS DAY")
+    print(f"[HIGHEST NET PROFIT SURPLUS] DAY: {sorted_surplus_list[0][1]}, AMOUNT: USD{sorted_surplus_list[0][0]}")
 elif len(deficit_list) == len(difference_list):
-    print("[CASH ON HAND SURPLUS] CASH ON HAND ON EACH DAY IS LOWER THAN PREVIOUS DAY")
-    print(f"[HIGHEST CASH ON HAND DEFICIT] DAY: {sorted_deficit_list[0][0]}, AMOUNT: {sorted_deficit_list[0][1]}")
+    print("[NET PROFIT SURPLUS] CASH ON HAND ON EACH DAY IS LOWER THAN PREVIOUS DAY")
+    print(f"[HIGHEST NET PROFIT DEFICIT] DAY: {sorted_deficit_list[0][1]}, AMOUNT: {sorted_deficit_list[0][0]}")
 elif len(surplus_list) == 0 and len(deficit_list) == 0:
     print("are you even playing the game?")
 else:
     for deficit in deficit_list:
         print(f"[CASH ON HAND DEFICIT] DAY: {deficit[0]}, AMOUNT: USD{deficit[1]}")
-    print(f"[HIGHEST CASH ON HAND DEFICIT] DAY: {sorted_deficit_list[0][0]}, AMOUNT: {sorted_deficit_list[0][1]}")
-    print(f"[2ND HIGHEST CASH ON HAND DEFICIT] DAY: {sorted_deficit_list[1][0]}, AMOUNT: {sorted_deficit_list[1][1]}")
-    print(f"[3RD HIGHEST CASH ON HAND DEFICIT] DAY: {sorted_deficit_list[2][0]}, AMOUNT: {sorted_deficit_list[2][1]}")
+    print(f"[HIGHEST CASH ON HAND DEFICIT] DAY: {sorted_deficit_list[0][1]}, AMOUNT: {sorted_deficit_list[0][0]}")
+    print(f"[2ND HIGHEST CASH ON HAND DEFICIT] DAY: {sorted_deficit_list[1][1]}, AMOUNT: {sorted_deficit_list[1][0]}")
+    print(f"[3RD HIGHEST CASH ON HAND DEFICIT] DAY: {sorted_deficit_list[2][1]}, AMOUNT: {sorted_deficit_list[2][0]}")
