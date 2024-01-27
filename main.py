2d792df6f0f37112766677d4c54d20fa40415247
jeiwy <156677793+jeiwy@users.noreply.github.com>
2024-01-27 23:37:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,12 +18,13 @@ with open(txtfile, mode='w') as file:
         file.writelines("[HIGHEST NET PROFIT DEFICIT] DAY: " + f"{cash_on_hand.deficit_list[0][0]}" + " AMOUNT: USD " + f"{cash_on_hand.deficit_list[0][1]}" )
     elif cash_on_hand.cash_flow_flag == 2:
         for record in cash_on_hand.deficit_list:
+            cash_on_hand.deficit_list.sort(key=itemgetter(0)) 
             file.writelines("[CASH DEFICIT] DAY: " + f"{record[0]}" + " AMOUNT: USD " + f"{record[1]}" + "\n")
         
         file.writelines("[HIGHEST CASH DEFICIT] DAY: " + f"{cash_on_hand.deficit_list[0][0]}" + " AMOUNT: USD " + f"{cash_on_hand.deficit_list[0][1]}" + "\n")
         file.writelines("[2ND HIGHEST CASH DEFICIT] DAY: " + f"{cash_on_hand.deficit_list[1][0]}" + " AMOUNT: USD " + f"{cash_on_hand.deficit_list[1][1]}" + "\n")
         file.writelines("[3RD HIGHEST CASH DEFICIT] DAY: " + f"{cash_on_hand.deficit_list[2][0]}" + " AMOUNT: USD " + f"{cash_on_hand.deficit_list[2][1]}" + "\n")
-        
+        cash_on_hand.deficit_list.sort(key=itemgetter(1)) 
 
 
     if profit_loss.profit_loss_flag == 0:
@@ -35,9 +36,9 @@ with open(txtfile, mode='w') as file:
     elif profit_loss.profit_loss_flag == 2:
         for record in profit_loss.deficit_list:
             file.writelines("[NET PROFIT DEFICIT] DAY: " + f"{record[0]}" + " AMOUNT: USD " + f"{record[1]}" + "\n")
+            profit_loss.deficit_list.sort(key=itemgetter(0)) to line 40
         
         file.writelines("[HIGHEST NET PROFIT DEFICIT] DAY: " + f"{profit_loss.deficit_list[0][0]}" + " AMOUNT: USD " + f"{profit_loss.deficit_list[0][1]}" + "\n")
         file.writelines("[2ND HIGHEST NET PROFIT DEFICIT] DAY: " + f"{profit_loss.deficit_list[1][0]}" + " AMOUNT: USD " + f"{profit_loss.deficit_list[1][1]}" + "\n")
         file.writelines("[3RD HIGHEST NET PROFIT DEFICIT] DAY: " + f"{profit_loss.deficit_list[2][0]}" + " AMOUNT: USD " + f"{profit_loss.deficit_list[2][1]}" + "\n")
-
-
+        cash_on_hand.deficit_list.sort(key=itemgetter(1)) 
